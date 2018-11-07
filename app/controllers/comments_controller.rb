a7aaa29aae0bee99187e39b094848136c5adc474
ryou_kuzuno <ryou0933@gmail.com>
2018-11-07 22:37:07 +0900

    form_forが動かない

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,10 @@
 class CommentsController < ApplicationController
   
   def reply
-    @comment = Comment.new(
-      comment: params[:comment],
-      book_id: params[:book_id],
-      user_id: params[:user_id],
-      displayable: params[:displayable]
-    )
-    @comment.save
-    redirect_to :action => "show/#{@book.id}"
-  end
-  
-  def create
-    comment = Comment.new(comment_params)
-    if comment.save
-      flash[:notice] = "コメントを投稿しました"
-      redirect_to comment.bookstore
-    begin
-      redirect_to :back, flash: {
-        comment: comment,
-        error_messages: comment.errors.full_messages
-      }
     
   end
+
+
 
   def destroy
     comment = Comment.find(params[:id])
