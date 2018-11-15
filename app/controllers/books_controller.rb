d074fbe6c8a8d53c11b14dc3842ef32073940ce6
TomoakiTANAKA <tanaka1987@gmail.com>
2018-11-15 22:07:42 +0900

    変数名を修正&localsでデータを渡すように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
             ).count
         # raise @user.inspect
         @new_comment = Comment.new
-        @comment = Comment.all
+        @comments = Comment.all
     end
 
     #新しく感想を投稿する画面のアクション
